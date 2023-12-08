8d13bea12bc5e0413739c2bd7609b1bf1920c2c7
ashwinbalaji <ashwinbalaji@infinitisoftware.net>
2023-12-09 02:25:37 +0530

    removed unwanted code.

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 `use strict`;
 let inputArr = [];
-const notNums = [
-  "+",
-  "-",
-  "/",
-  "*",
-  "%",
-  "clear",
-  "delete",
-  "minus",
-  "calculate",
-];
 const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const operations = ["+", "-", "/", "*", "%"];
 const functionalities = ["clear", "delete", "minus", "calculate"];
